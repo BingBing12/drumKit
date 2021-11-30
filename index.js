eeff2a9c8f92dc562e5d5a54ae0d9f17a380f28c
Robin Tainton <robintainton0509@gmail.com>
2021-12-01 01:46:51 +0200

    add all files

---
+++ index.js
@@ -0,0 +1,61 @@
+//event listener
+var button = document.querySelectorAll(".drum")
+for(var i=0;i<button.length;i++){
+    button[i].addEventListener("click", handle)
+}
+
+function handle(){
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+}
+
+document.addEventListener("keydown", function(e){
+   makeSound(e.key);
+   buttonAnimation(e.key);
+})
+
+function makeSound(key){
+    switch(key){
+        case "w": 
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'a': 
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 's': 
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'd': 
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'j': 
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'k': 
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'l': 
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default:
+            console.log(key)
+    }
+}
+
+function buttonAnimation(key){
+    var button = document.querySelector("." + key);
+    
+    button.classList.add("pressed");
+    setTimeout(function(){
+        button.classList.remove("pressed");
+    }, 100);
+    
+}
+
